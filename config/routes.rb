792f8940f5a16fd404e0ae4bb853e956ad9ac443
aaltmark <adamaltmark@gmail.com>
2020-06-24 14:54:55 -0400

    first edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :artists, only: [:index, :show, :new, :edit]
+
+  patch '/artists/:id', to: 'artists#update'
+  
+  post '/artists', to: 'artists#create'
 end
